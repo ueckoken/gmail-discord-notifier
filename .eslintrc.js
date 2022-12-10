cf1d2fcec608789f9477151ad9579eb86b7b880c
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-12-10 16:22:44 +0900

    add gts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: "standard-with-typescript",
+  extends: 'standard-with-typescript',
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     project: [
-      "tsconfig.json"
+      'tsconfig.json'
     ]
   },
-  rules: {},
-};
+  rules: {}
+}
